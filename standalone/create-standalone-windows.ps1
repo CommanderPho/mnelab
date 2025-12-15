29f0ea388846f5436f32dd6cef67938d384c9f9b
Pho Hale <PhoHale@gmail.com>
2025-12-15 16:44:59 -0500

    bump

--- standalone/create-standalone-windows.ps1
+++ standalone/create-standalone-windows.ps1
@@ -11,4 +11,4 @@ pyinstaller `
     --icon ..\src\mnelab\icons\mnelab-logo.ico `
     ..\src\mnelab\__main__.py
 
-& iscc.exe /Dversion=$(python get_version.py) mnelab-windows.iss
+# & iscc.exe /Dversion=$(python get_version.py) mnelab-windows.iss
